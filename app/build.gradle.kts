106011ef5955eb6fd0b21f2283bc811e072e91ca
Kot512 <kelohov512@gmail.com>
2025-01-10 21:27:11 +0300

    target and compile sdk updated to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.kostuciy.grasstouchtracker"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.kostuciy.grasstouchtracker"
         minSdk = 27
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
